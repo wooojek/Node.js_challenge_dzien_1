7fcfc6f6422702e2b04a78c9641949411d1ab59f
wooojek <yokomura123@gmail.com>
2018-01-29 21:33:05 +0100

    day assignment finished

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,12 @@
-//Twój kod
+let numbersArray = [...process.argv];
+numbersArray.splice(0,2);
+
+let sleepSort = (array) => {
+    array.forEach((element) => {
+        let parsedElement = Number(element);
+        let timeout = parsedElement*1000;
+        setTimeout(() => console.log(parsedElement), timeout);
+    });
+};
+
+sleepSort(numbersArray);
