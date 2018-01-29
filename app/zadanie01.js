2eadee0d0b34dd9a8777c8bc92c8af04bab83277
wooojek <yokomura123@gmail.com>
2018-01-29 21:02:58 +0100

    first and second assignment done

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,4 @@
-//Twój kod
+console.log('Michal Wooojek');
+setTimeout(() => {
+    console.log('wita się z Node.js!');
+}, 5000);
--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,14 @@
-//Twój kod
+function logOnTimeout (text, delay) {
+    setTimeout(() => {
+        console.log(text);
+    }, delay);
+};
+
+logOnTimeout('Node.js', 4000);
+logOnTimeout('się', 1000);
+logOnTimeout('Witam', 0);
+logOnTimeout('i korzystam', 6000);
+logOnTimeout('w konsoli', 5000);
+logOnTimeout('z funkcji czasu!', 7000);
+logOnTimeout('z', 2000);
+logOnTimeout('programem', 3000);
